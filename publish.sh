e9fa054d031f747c55526c57d2f1d858bcb2c5f1
Nathan Bellowe <nbellowe@gmail.com>
2015-09-15 18:03:39 -0600

    Update publish

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 git checkout gh-pages
-rmf ^_site*
+rm -rf ^_site*
 mv _site/* .
-rmf _site
-git a *
+rm -rf _site
+git add .
 git push gh-pages
 git commit -m "Check in built version"
 git push origin gh-pages
