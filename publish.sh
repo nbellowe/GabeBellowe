809a602b500e84118e32a3da427cf90bd986a766
Nathan Bellowe <nbellowe@gmail.com>
2015-09-15 18:38:49 -0600

    Check in built version

--- publish.sh
+++ publish.sh
@@ -1,8 +1,9 @@
-git checkout master
 jekyll build
 rm -rf ^_site
 mv _site/* .
+rm -rf _site
 git checkout gh-pages
 git add .
 git commit -m "Check in built version"
-git push origin gh-pages 3035  git co master
+git push origin gh-pages
+git checkout master
